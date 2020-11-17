69bc0cb5705262098d4eeb93612948c3be0cc263
JandreErasmus <jandreerasmus396@gmail.com>
2020-11-17 15:27:01 +0200

    added heroku

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Fileupload from './components/Fileupload';
+
+function App() {
+  return (
+    <div className="container mt-4">
+     <h4 className="display-4 text-center mb-4">
+      File Upload
+    </h4>
+    <Fileupload></Fileupload>
+    </div>
+  );
+}
+
+export default App;
+
